7f0b5a5122836a64927f10195f87e362f288c34a
pavan87779 <pavanbabu860@gmail.com>
2025-09-04 20:29:20 +0530

    daily updates

---
+++ python/if_nested_if_statements.py
@@ -0,0 +1,22 @@
+mat = int(input("enter your maths marks:"))
+phy = int(input("enter your physics marks:"))
+che = int(input("enter your chemistry marks:"))
+if mat>=35 and phy>=35 or che >=35:
+    print("pass")
+else:
+    print("fail")
+print("-----------------------------------")
+a = 100
+b=20
+c =300
+if a>b:
+    if a>c:
+        print(a)
+    else:
+        print(c)
+else:
+    if b>c:
+        print(b)
+    else:
+        print(c)
+            
