f42f7ef3ddeede8fa556d67bbb3f761df7c66edf
pavan87779 <pavanbabu860@gmail.com>
2025-09-05 19:25:21 +0530

    daily progress

--- Problem Solving/Whats_ur_name.py
+++ Problem Solving/Whats_ur_name.py
@@ -14,7 +14,7 @@ def print_full_name(first, last):
 if __name__ == '__main__':
     first_name = input()
     last_name = input()
-    print_full_name(first_name, last_name)
+    print_full_name(first_name, last_name)k
 
 """
 You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
