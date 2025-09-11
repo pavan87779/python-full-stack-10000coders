ee46a19b37dd9e34bf708a0315f4a3eb30b4522b
pavan87779 <pavanbabu860@gmail.com>
2025-09-11 19:21:54 +0530

    progress

---
+++ python/pattern_printings.py
@@ -0,0 +1,26 @@
+n = 6
+for i in range(1,n):
+    for j in range(n):
+        print("*", end=" ")
+    print("")
+"""
+******
+******
+******
+******
+******
+"""
+
+n = 5
+for i in range(1,n):
+    for j in range(1,i+1):
+        print("*", end=" ")
+    print("")
+
+"""
+* 
+* * 
+* * * 
+* * * * 
+"""
+
